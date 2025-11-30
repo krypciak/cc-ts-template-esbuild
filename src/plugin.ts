053bcded0d29f896cf7838eaf30c43924f7272a1
krypek <krypek@tuta.io>
2025-11-30 11:53:04 +0100

    Add verbatimModuleSyntax to tsconfig.json

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,9 +1,11 @@
-import { PluginClass } from 'ultimate-crosscode-typedefs/modloader/mod'
-import { Mod1 } from './types'
+import type { PluginClass } from 'ultimate-crosscode-typedefs/modloader/mod'
+import type { Mod1 } from './types'
+import ccmod from '../ccmod.json'
 
 export default class TsTemplateEsbuild implements PluginClass {
     static dir: string
     static mod: Mod1
+    static manifset: typeof import('../ccmod.json') = ccmod
 
     constructor(mod: Mod1) {
         TsTemplateEsbuild.dir = mod.baseDirectory
