cd317175675b1ab6d3c5b0eb3fcd1801eca5a3ac
krypek <krypek@tuta.io>
2026-07-22 23:01:41 +0200

    Extract mod metadata from main plugin class to seperate importable object

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,17 +1,10 @@
 import type { PluginClass } from 'ultimate-crosscode-typedefs/modloader/mod'
 import type { Mod1 } from './types'
-import ccmod from '../ccmod.json'
+import { setModMetadata } from './mod-metadata'
 
 export default class TsTemplateEsbuild implements PluginClass {
-    static dir: string
-    static mod: Mod1
-    static manifset: typeof import('../ccmod.json') = ccmod
-
     constructor(mod: Mod1) {
-        TsTemplateEsbuild.dir = mod.baseDirectory
-        TsTemplateEsbuild.mod = mod
-        TsTemplateEsbuild.mod.isCCL3 = mod.findAllAssets ? true : false
-        TsTemplateEsbuild.mod.isCCModPacked = mod.baseDirectory.endsWith('.ccmod/')
+        setModMetadata(mod)
     }
 
     async prestart() {}
